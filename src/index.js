e98ea7b50fead07c362445b4f06cbfdbd96a2d4e
YanPanMichael <yanpancsu@icloud.com>
2018-07-29 18:38:53 +0800

    add middlewares thunk and compose of redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createLogger } from 'redux-logger'
 import rootReducer from './reducers'
 import './index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 
-const store = createStore(rootReducer)
+var middlewares = [thunk];
+if(process.env.NODE_ENV !== 'production') {
+    middlewares.push(createLogger());
+}
+
+// 柯里化
+// https://segmentfault.com/a/1190000008754562
+const store = compose(applyMiddleware(...middlewares))(createStore)(rootReducer)
+
 ReactDOM.render(
     <Provider store={store}>
         <App />
