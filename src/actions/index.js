eebfbf00a0895b211ec64027491809c0d6d1b4df
YanPanMichael <yanpancsu@icloud.com>
2018-08-31 00:36:04 +0800

    add and utilize redux thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import fetch from 'cross-fetch'
+
 let nextTodoId = 0
 export const addTodo = text => ({
     type: 'ADD_TODO',
@@ -19,4 +21,30 @@ export const VisibilityFilters = {
     SHOW_ALL: 'SHOW_ALL',
     SHOW_COMPLETED: 'SHOW_COMPLETED',
     SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
+
+
+export const updateSomeStatusByThunk = (para) => dispatch => {
+    dispatch({
+        type: 'UPDATE_SOMESTATUS',
+        para
+    })
+}
+//this.props.dispatch(updateSomeStatusByThunk(value))
+
+export const updateSomeStatusByThunk = (id) => dispatch => {
+    let url = `/aaa/bbb?ccc=json&_id=${id}&source=xxx`;
+    fetch(url).then(response => {
+        return response.json();
+    }).then(data => {
+        let jsonVar = {...data.mainjson, id: data.srcId};
+        let list = data.results.list.slice(0, 10);
+        let arraylist = [jsonVar, ...list];
+        dispatch({
+            type: LOADSUCCESS,
+            arraylist
+        });
+    }).catch(err => {
+        console.error(err);
+    });
 }
