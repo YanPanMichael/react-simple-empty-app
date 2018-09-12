935cb2816efbc354fa76fe6146118e555d4579fa
YanPanMichael <yanpancsu@icloud.com>
2018-09-12 22:01:17 +0800

    add babel-polyfill

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import 'babel-polyfill'
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
